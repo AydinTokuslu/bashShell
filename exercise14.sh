54363b01486b8700c757be0f42ffa4b7fc7ea8a0
AydinTokuslu <aydintokuslu78@gmail.com>
2024-12-13 18:21:43 +0100

    exercise14

--- exercise14.sh
+++ exercise14.sh
@@ -6,3 +6,10 @@ num1=$1
 num2=$2
 
 echo "Number of Paramaters: "$#
+echo "Current ID Process: "$$
+echo "Double Quotation Parameters: "$*
+
+((sum=$mum1+$num2))
+echo $sum
+
+echo "Exit Status: "$?
