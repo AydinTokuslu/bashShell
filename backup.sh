b9353d2874a63ead581373b7ca8f787b3e25528d
AydinTokuslu <aydintokuslu78@gmail.com>
2024-12-16 17:52:33 +0100

    backup

---
+++ backup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+`tar cvf /home/useradmin/backup/backup.tar /var/backups`
+
+`find backup.tar.gz -mtime -1 -type f -print &> /dev/null`
+
+`gzip /home/useradmin/backup/backup.tar`
+
+if [ $? -eq 0 ]
+then
+	
+	echo "Backup was created"
+	echo
+	echo "Archiving backup"
+	scp /tmp/backup.tar.gz root@192.168.1.x:/path
+else
+	echo "Backup Failed"
+fi
+
+ls -lts /home/useradmin/backup/
