ac09e158d9ca5f222099ddf9601c9d558d237c42
AydinTokuslu <aydintokuslu78@gmail.com>
2024-12-22 12:44:17 +0100

    check disk space status

--- disk-space-status.sh
+++ disk-space-status.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 
+chkSpace=df -h | egrep -v "tmpfs" | tail -n+2 | awk '{print $5}' | cut -d'%' -f1
+
+for i in $chkSpace
+do
+
+    if [ $i -ge 21 ]
+    then
+        echo "Space is greater than 21%"
+    fi
+
+done
