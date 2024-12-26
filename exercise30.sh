a1d3ea0862ec9a922ea4f2c7299ed6a9d45c938c
AydinTokuslu <aydintokuslu78@gmail.com>
2024-12-26 17:29:49 +0100

    exercise30

--- exercise30.sh
+++ exercise30.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-#
+# Ask user to enter two numbers to variables num1 and num2.
+# Calculate the total of 2 numbers.
+# 3. Print the total number and increase it by 1.
+# 4. Print the new value of the total number.
+# 5. Subtract num1 from the total number and print result.
+# 6. Change the num1 and num2 variables to be passed from the Command 
+# line arguments instead of receiving them from the user
+
+read -p "enter first number" num1
+read -p "enter second number" num2
+
+((sum=$num1+$num2))
+"Total number is $sum"
+let sum++
+"Total number is $sum"
