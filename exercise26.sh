e7570574c32736f38c4aebcf0ae6f1567598ac4d
AydinTokuslu <aydintokuslu78@gmail.com>
2024-12-26 11:20:12 +0100

    exercise26

--- exercise26.sh
+++ exercise26.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 echo "This is my first script"
+
+#echo "ENTER YOUR NAME: "
+#read name
+
+#read -p "enter your name: " name
+#echo Hello $name
+read -s -p "enter your passwd: " pswd
+echo $pswd
