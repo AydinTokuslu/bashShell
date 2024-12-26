0170c09fd8fdd95c331da812a0778928c9aa4512
AydinTokuslu <aydintokuslu78@gmail.com>
2024-12-26 19:16:35 +0100

    case statements

---
+++ exercise35-case-statement.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+read -p "Input first number: " first_number
+read -p "Input second number: " second_number
+read -p "Select an math operation
+1 - addition
+2 - subtraction
+3 - multiplication
+4 - division
+" operation
+
