a75996b02abfd49b2a8c6c27bf49e4ea5b85ca21
AydinTokuslu <aydintokuslu78@gmail.com>
2024-12-26 16:48:20 +0100

    exercise29 arithmetic

--- exercise28-let.sh
+++ exercise28-let.sh
@@ -3,12 +3,20 @@
 num1=30
 num2=5
 
-let total=num1+num2
-let diff=num1-num2
-let mult=num1*num2
-let div=num1/num2
+let "total = $num1 + $num2"
+let "diff = $num1 - $num2"
+let "mult = $num1 * $num2"
+let "div = $num1 / $num2"
 
 echo "Total= $total"
 echo "Difference= $diff"
 echo "Multiple= $mult"
 echo "Division= $div"
+
+x=5
+let x++
+echo $x
+
+y=3
+let y--
+echo $y
