4bdf7650fe1ca66a2a5fe82dfffc1e335fb1d2af
AydinTokuslu <aydintokuslu78@gmail.com>
2024-12-26 11:29:38 +0100

    exercise26

--- exercise26.sh
+++ exercise26.sh
@@ -7,5 +7,13 @@ echo "This is my first script"
 
 #read -p "enter your name: " name
 #echo Hello $name
-read -s -p "enter your passwd: " pswd
-echo $pswd
+read -p "enter your passwd: " pswd
+echo $pswd 
+echo "The name of the Bash script: " $0 
+echo "The first argument : " $1 
+echo "How many arguments were passed to the Bash script: " $#
+echo "All the arguments: " $@
+echo "The exit status of the most recently run process: " $?
+echo "The process ID of the current script: " $$
+echo "The username of the user running the script: " $USER
+echo "The hostname of the machine the script is running on: " $HOSTNAME
