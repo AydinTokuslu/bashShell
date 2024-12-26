0ef463aad58b95647878d31d7592a0a00dce270f
AydinTokuslu <aydintokuslu78@gmail.com>
2024-12-26 11:42:35 +0100

    exercise28 let

---
+++ exercise28-let.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+num1=30
+num2=5
+
+let total=num1+num2
+let diff=num1-num2
+let mult=num1*num2
+let div=num1/num2
+
+echo "Total= $total"
+echo "Difference= $diff"
+echo "Multiple= $mult"
+echo "Division= $div"
