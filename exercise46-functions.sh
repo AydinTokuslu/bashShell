f8d3e9a519085be8a5fa6241dcd5cbc286f5ff84
AydinTokuslu <aydintokuslu78@gmail.com>
2024-12-27 18:00:20 +0100

    birth day calculation

--- exercise46-functions.sh
+++ exercise46-functions.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Create a function named print_age that accepts one argument
+# Ask user to input his/her year of birth and store it to local birth_year variable
+# Calculate age using current year value from the first argument
+# Print age with a message
+# Call print_age function with 2022
+
+
 function print_age () {
     #read -p "enter your birtday : " birth
     read -p "enter your birtday : " $1
